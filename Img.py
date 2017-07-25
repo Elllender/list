15255062d1cd85cba65699c283cb0591e8c4148c
Elllender <frimov2017@yandex.ru>
2017-07-26 00:00:32 +0300

    Update Img.py

--- Img.py
+++ Img.py
@@ -1,1 +1,18 @@
+from PIL import Image
 
+img1 = Image.open('img1.jpg')
+img2 = Image.open('img2.jpg')
+
+size = img1.size
+
+mas1 = []
+mas2 = []
+
+for x in range(0,size[0]):
+    for y in range(0, size[1]):
+        color = img1.getpixel((x,y))
+        mas1.append(color)
+        color2 = img2.getpixel((x,y))
+        mas2.append(color2)
+
+mas1.sort
